a8d843cec666b3fc09bb83cca91998365d4a034f
Gispsy <mordredbta3011@gmail.com>
2023-01-11 15:01:54 +0100

    affichage Color OK

--- src/main/java/fr/mixercolor/colormixer/ColorMixerController.java
+++ src/main/java/fr/mixercolor/colormixer/ColorMixerController.java
@@ -3,10 +3,11 @@ package fr.mixercolor.colormixer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 
 public class ColorMixerController {
 
-    //Function Label and slider
+    // Label and slider
     public Slider sldRed;
     public Slider sldGreen;
     public Slider sldBlue;
@@ -15,5 +16,40 @@ public class ColorMixerController {
     public Label lblBlue;
     public Label lblMix;
 
-    
+    //Ghost value
+    public double red;
+    public double green;
+    public double blue;
+
+    //Function
+    public void moussReleasedRed(MouseEvent mouseEvent) {
+        sldRed.getValue();
+        red = sldRed.getValue();
+
+        //System.out.println(sldRed.getValue());
+        changeStyle();
+
+    }
+
+    public void moussReleasedGreen(MouseEvent mouseEvent) {
+        sldGreen.getValue();
+        green = sldGreen.getValue();
+
+        //System.out.println(sldGreen.getValue());
+        changeStyle();
+
+    }
+
+    public void moussReleasedBlue(MouseEvent mouseEvent) {
+        sldBlue.getValue();
+        blue = sldBlue.getValue();
+
+        //System.out.println(sldBlue.getValue());
+        changeStyle();
+    }
+
+    public void changeStyle(){
+        lblMix.setStyle("-fx-background-color: rgb("+red+","+green+","+blue+")");
+
+    }
 }
